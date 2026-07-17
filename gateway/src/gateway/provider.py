f33e314a431fcbb8cd05dff4abd65f08b9c67181
Matt Galvin <matthewgalvin908@gmail.com>
2026-07-17 13:41:46 +0200

    fmt

--- gateway/src/gateway/provider.py
+++ gateway/src/gateway/provider.py
@@ -4,5 +4,4 @@ from .models import GenerateRequest, GenerateResponse
 
 
 class ModelProvider(Protocol):
-    def generate(self, request: GenerateRequest) -> GenerateResponse:
-        ...
+    def generate(self, request: GenerateRequest) -> GenerateResponse: ...
